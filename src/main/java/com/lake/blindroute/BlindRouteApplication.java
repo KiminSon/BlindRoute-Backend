c957ed84079b0598a1b7259ac9614554b0703fef
KiminSon <kimin0331@naver.com>
2024-05-26 15:46:23 +0900

    Initial commit

---
+++ src/main/java/com/lake/blindroute/BlindRouteApplication.java
@@ -0,0 +1,13 @@
+package com.lake.blindroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlindRouteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lake/blindroute/BlindRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lake.blindroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindRouteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
